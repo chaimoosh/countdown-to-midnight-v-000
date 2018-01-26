b6a7eacc5fa431a737b959de36a67b06f4a6c107
Chaim Reisman <crreisman@gmail.com>
2018-01-26 14:30:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
-
-def countdown
+  
+def countdown(seconds)
+  #seconds = 5
+  while seconds > 0 
+    puts " #{seconds} SECOND(S)!"
+    seconds -= 1 
+  puts "HAPPY NEW YEAR!"
+  break
+  end
 end
